6c94331dc6a1221afb887581e3f6144db636de13
h1634668150 <1634668150@qq.com>
2019-01-23 11:52:09 +0800

    上传文件夹

--- lagou_common/src/main/java/com/lagou/mapper/CompanyStageMapper.java
+++ lagou_common/src/main/java/com/lagou/mapper/CompanyStageMapper.java
@@ -1,30 +1,12 @@
 package com.lagou.mapper;
 
-import com.lagou.common.pojo.CompanyStage;
-import com.lagou.common.pojo.CompanyStageExample;
-import java.util.List;
+
+import com.lagou.pojo.CompanyStage;
+import com.lagou.pojo.CompanyStageExample;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 public interface CompanyStageMapper {
-    int countByExample(CompanyStageExample example);
 
-    int deleteByExample(CompanyStageExample example);
-
-    int deleteByPrimaryKey(Integer id);
-
-    int insert(CompanyStage record);
-
-    int insertSelective(CompanyStage record);
-
-    List<CompanyStage> selectByExample(CompanyStageExample example);
-
-    CompanyStage selectByPrimaryKey(Integer id);
-
-    int updateByExampleSelective(@Param("record") CompanyStage record, @Param("example") CompanyStageExample example);
-
-    int updateByExample(@Param("record") CompanyStage record, @Param("example") CompanyStageExample example);
-
-    int updateByPrimaryKeySelective(CompanyStage record);
-
-    int updateByPrimaryKey(CompanyStage record);
 }
