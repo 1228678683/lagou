aecb3caa2aabcd16ee6a2d2da42d1c7d2b731841
CloudZQJoe <46859349+CloudZQJoe@users.noreply.github.com>
2019-01-24 11:58:45 +0800

    Update and rename sso_provider_8083.java to sso_provider_8000.java

--- lagou_sso_provider/src/main/java/com/lagou/sso_provider_8000.java
+++ lagou_sso_provider/src/main/java/com/lagou/sso_provider_8000.java
@@ -9,8 +9,8 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 //@MapperScan("com.lagou.company.mapper")//创建自己的包
 @EnableEurekaClient
 @EnableHystrix        //开启断路器机制
-public class sso_provider_8083 {
+public class sso_provider_8000 {
     public static void main(String[] args) {
-        SpringApplication.run(sso_provider_8083.class);
+        SpringApplication.run(sso_provider_8000.class);
     }
 }
