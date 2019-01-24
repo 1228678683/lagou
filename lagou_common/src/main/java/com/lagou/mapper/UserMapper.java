ebefac64c4b306b453b0531e7b5fb203053337f4
CloudZQJoe <46859349+CloudZQJoe@users.noreply.github.com>
2019-01-24 11:50:13 +0800

    Update UserMapper.java

--- lagou_common/src/main/java/com/lagou/mapper/UserMapper.java
+++ lagou_common/src/main/java/com/lagou/mapper/UserMapper.java
@@ -1,9 +1,16 @@
 package com.lagou.mapper;
 
 
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.lagou.pojo.CompanyBasic;
 import com.lagou.pojo.User;
 
 public interface UserMapper extends BaseMapper<User> {
+  @Select("select count(1) from tb_user where ${paramType}=#{param}")
+	Integer selectCount1(
+		@Param("param")	String param, 
+		@Param("paramType")	String paramType);
+
 }
